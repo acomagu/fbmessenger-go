1e5cbe97a3673aa6a1403960c7b731338bbd3f5d
kurouw <github0220@gmail.com>
2016-05-07 21:34:28 +0900

    change

--- fbmessenger.go
+++ fbmessenger.go
@@ -102,7 +102,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		messagingEvents := receivedMessage.Entry[0].Messaging
 		for _, event := range messagingEvents {
 			if &event.Message != nil && event.Message.Text != "" {
-				handleReceiveFacebookMessage(event)
+				HandleReceiveFacebookMessage(event)
 			}
 		}
 		fmt.Fprintf(w, "Success")
