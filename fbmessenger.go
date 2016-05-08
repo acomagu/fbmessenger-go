5500d74eba90563ecf489d22db0f341a178ea4ba
Yuki Ito <acomagu@gmail.com>
2016-05-08 19:15:06 +0900

    WIP

--- fbmessenger.go
+++ fbmessenger.go
@@ -11,6 +11,7 @@ import (
 	"net/url"
 	"os"
 	"time"
+	"errors"
 )
 
 var accessToken = os.Getenv("ACCESS_TOKEN")
@@ -21,25 +22,25 @@ const (
 	EndPoint = "https://graph.facebook.com/v2.6/me/messages"
 )
 
-// ReceivedMessage ...
-type ReceivedMessage struct {
+// receivedMessage ...
+type receivedMessage struct {
 	Object string  `json:"object"`
-	Entry  []Entry `json:"entry"`
+	Entry  []entry `json:"entry"`
 }
 
-// Entry ...
-type Entry struct {
-	ID        int64       `json:"id"`
-	Time      int64       `json:"time"`
-	Messaging []Messaging `json:"messaging"`
+// entry ...
+type entry struct {
+	ID        int64        `json:"id"`
+	Time      int64        `json:"time"`
+	Messagings []messaging `json:"messaging"`
 }
 
-// Messaging ...
-type Messaging struct {
+// messaging ...
+type messaging struct {
 	Sender    Sender    `json:"sender"`
-	Recipient Recipient `json:"recipient"`
+	Recepient Recepient `json:"recipient"`
 	Timestamp int64     `json:"timestamp"`
-	Message   Message   `json:"message"`
+	Message   message   `json:"message"`
 }
 
 // Sender ...
@@ -47,27 +48,48 @@ type Sender struct {
 	ID int64 `json:"id"`
 }
 
-// Recipient ...
-type Recipient struct {
+// Recepient ...
+type Recepient struct {
 	ID int64 `json:"id"`
 }
 
-// Message ...
-type Message struct {
+// message ...
+type message struct {
 	MID  string `json:"mid"`
 	Seq  int64  `json:"seq"`
 	Text string `json:"text"`
 }
 
-// SendMessage ...
-type SendMessage struct {
-	Recipient Recipient `json:"recipient"`
+// sendMessage ...
+type sendMessage struct {
+	Recepient Recepient `json:"recipient"`
 	Message struct {
 		Text string `json:"text"`
 	} `json:"message"`
 }
 
-func Listen(callback func(Messaging)) {
+// Event express one messaging
+type Event struct {
+	Sender Sender
+	Recepient Recepient
+	Content interface{}
+}
+
+// TextContent express content of one message
+type TextContent struct {
+	Text string
+}
+
+func (_messaging *messaging) Event(_recepient Recepient) Event {
+	event := Event{}
+	event.Sender = _messaging.Sender
+	event.Recepient = _recepient
+	event.Content = TextContent{_messaging.Message.Text}
+	return event
+}
+
+// Listen call callback function given when requested from Facebook service.
+func Listen(callback func(Event)) {
 	handleReceiveMessage = callback
 	http.HandleFunc("/", webhookHandler)
 	http.HandleFunc("/webhook", webhookHandler)
@@ -76,11 +98,7 @@ func Listen(callback func(Messaging)) {
 	http.ListenAndServe(addr, nil)
 }
 
-var handleReceiveMessage func(Messaging)
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Facebook Bot")
-}
+var handleReceiveMessage func(Event)
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -91,7 +109,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if r.Method == "POST" {
-		var receivedMessage ReceivedMessage
+		var receivedMessage receivedMessage
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Print(err)
@@ -99,19 +117,30 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		if err = json.Unmarshal(b, &receivedMessage); err != nil {
 			log.Print(err)
 		}
-		messagingEvents := receivedMessage.Entry[0].Messaging
+		messagingEvents := receivedMessage.Entry[0].Messagings
 		for _, event := range messagingEvents {
 			if &event.Message != nil && event.Message.Text != "" {
-				handleReceiveMessage(event)
+				handleReceiveMessage(event.Event(Recepient{0}))
 			}
 		}
 		fmt.Fprintf(w, "Success")
 	}
 }
 
-func SendTextMessage(recipient Recipient, sendText string) {
-	m := new(SendMessage)
-	m.Recipient = recipient
+// Send method send Event
+func Send(event Event) error {
+	switch content := event.Content.(type) {
+	case TextContent:
+		sendTextMessage(event.Recepient, content.Text)
+	default:
+		return errors.New("Event.Content type is invalid")
+	}
+	return nil
+}
+
+func sendTextMessage(recepient Recepient, sendText string) {
+	m := new(sendMessage)
+	m.Recepient = recepient
 
 	log.Print("------------------------------------------------------------")
 	log.Print(m.Message.Text)
